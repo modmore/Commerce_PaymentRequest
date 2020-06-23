796dc1ff71a77c39b743823b6034dc967e70a929
Mark Hamstra <hello@markhamstra.com>
2020-06-23 13:03:35 +0200

    build

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -24,7 +24,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     define('PKG_NAME', 'Commerce Payment Request');
     define('PKG_NAMESPACE', 'commerce_paymentrequest');
     define('PKG_VERSION', '1.0.0');
-    define('PKG_RELEASE', 'rc2');
+    define('PKG_RELEASE', 'pl');
 
     /* load modx */
     require_once dirname(__DIR__) . '/config.core.php';
