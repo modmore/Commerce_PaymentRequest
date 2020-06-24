192b4cf9805479fdbd25b506a2665901a028bd80
Mark Hamstra <hello@markhamstra.com>
2020-06-24 23:43:38 +0200

    Build release

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,7 +23,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME', 'Commerce Payment Request');
     define('PKG_NAMESPACE', 'commerce_paymentrequest');
-    define('PKG_VERSION', '1.0.0');
+    define('PKG_VERSION', '1.1.0');
     define('PKG_RELEASE', 'pl');
 
     /* load modx */
